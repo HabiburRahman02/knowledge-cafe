adf5040e4d72ab22ef305d36afa67cb57b87aeeb
Habibur Rahman <habibur88454@gmail.com>
2024-10-21 21:56:15 +0600

    initial setup done

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div>
+            <h2 className="text-4xl font-semibold">Knowledge Cafe</h2>
+        </div>
+    );
+};
+
+export default Header;
